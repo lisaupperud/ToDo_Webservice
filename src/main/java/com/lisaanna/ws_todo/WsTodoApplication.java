ad746dc2ba859b30d7b4c9e1877a2ac3ea368654
liluppis <lisa.upp@hotmail.com>
2025-09-05 10:25:34 +0200

    initial commit

---
+++ src/main/java/com/lisaanna/ws_todo/WsTodoApplication.java
@@ -0,0 +1,13 @@
+package com.lisaanna.ws_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisaanna/ws_todo/WsTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisaanna.ws_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
