0bc4a9c20d90fe162f040ca4104e305c73202a7b
liluppis <lisa.upp@hotmail.com>
2025-09-05 10:47:41 +0200

    add: Node.js and MongoDB connection

--- src/main/java/com/lisaanna/ws_todo/WsTodoApplication.java
+++ src/main/java/com/lisaanna/ws_todo/WsTodoApplication.java
@@ -3,7 +3,9 @@ package com.lisaanna.ws_todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class WsTodoApplication {
 
 	public static void main(String[] args) {
