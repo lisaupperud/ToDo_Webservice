97b54720441e9e493f98d158bea0dedf0a6ea9c0
Anna Wallström <anoli89@hotmail.com>
2025-09-08 09:09:07 +0200

    Add annotations in Controller class

--- src/main/java/com/lisaanna/ws_todo/controller/TaskController.java
+++ src/main/java/com/lisaanna/ws_todo/controller/TaskController.java
@@ -1,4 +1,26 @@
 package com.lisaanna.ws_todo.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController //suggestion: "/tasks/"?
 public class TaskController {
+
+    // get all: suggestion "/"?
+    //@GetMapping
+
+    // get filtered
+    //@GetMapping
+
+    // create new
+    //@PostMapping
+
+    // update
+    //@PutMapping
+
+    //delete specific
+    //@DeleteMapping
+
+    // delete all completed
+    //@DeleteMapping
+
 }
