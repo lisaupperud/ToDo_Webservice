ff4e8de400ebac6b76882ff6643c861d56439b85
liluppis <lisa.upp@hotmail.com>
2025-09-08 10:33:17 +0200

    delete: unused import

--- src/main/java/com/lisaanna/ws_todo/entity/Task.java
+++ src/main/java/com/lisaanna/ws_todo/entity/Task.java
@@ -1,6 +1,5 @@
 package com.lisaanna.ws_todo.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
