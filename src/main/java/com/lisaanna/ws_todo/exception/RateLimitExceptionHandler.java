467830598f6c9c10e499669f1ff42963513885e0
liluppis <lisa.upp@hotmail.com>
2025-09-11 15:00:51 +0200

    fix: issue with RateLimiter. Move rate limiter config into application.properties and removed method rateLimiter()

---
+++ src/main/java/com/lisaanna/ws_todo/exception/RateLimitExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.lisaanna.ws_todo.exception;
+
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// To handle exception thrown when limit for requests are reached
+@ControllerAdvice
+public class RateLimitExceptionHandler {
+
+    @ExceptionHandler(RequestNotPermitted.class)
+    public ResponseEntity<Map<String, Object>> handleRequestNotPermitted(RequestNotPermitted e, HttpServletRequest request) {
+
+        // Using a HashMap for key-values pairs
+        // Output is now in JSON format for clearer output message when exception is thrown
+        Map<String, Object> jsonBody = new HashMap<>();
+        jsonBody.put("timestamp", System.currentTimeMillis());
+        jsonBody.put("status", HttpStatus.TOO_MANY_REQUESTS.value());
+        jsonBody.put("error", "Too Many Requests Attempted");
+        jsonBody.put("message", "Rate limit reached, try again later");
+        jsonBody.put("path", request.getRequestURI()); // so we can get the URI from the request
+
+        return ResponseEntity
+                .status(HttpStatus.TOO_MANY_REQUESTS)
+                .body(jsonBody);
+    }
+}
--- src/main/java/com/lisaanna/ws_todo/security/SecurityConfig.java
+++ src/main/java/com/lisaanna/ws_todo/security/SecurityConfig.java
@@ -1,22 +1,8 @@
 package com.lisaanna.ws_todo.security;
 
-import io.github.resilience4j.ratelimiter.RateLimiterConfig;
-import io.github.resilience4j.ratelimiter.RateLimiter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public RateLimiter rateLimiter() {
-        RateLimiterConfig config = RateLimiterConfig.custom()
-                .limitForPeriod(2) // Max requests per interval
-                .limitRefreshPeriod(Duration.ofMinutes(1)) // Interval duration
-                .timeoutDuration(Duration.ofSeconds(1)) // Timeout for acquiring permits
-                .build();
-        return RateLimiter.of("myRateLimiter", config);
-    }
+    // class can safely be deleted, unless we want to add more security configurations
 }
