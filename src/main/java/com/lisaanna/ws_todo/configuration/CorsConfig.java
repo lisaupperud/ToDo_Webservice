19fb86758b597d4bcbc5c38bbe6a28b4e282d424
liluppis <lisa.upp@hotmail.com>
2025-09-17 15:20:04 +0200

    add: cors config

---
+++ src/main/java/com/lisaanna/ws_todo/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.lisaanna.ws_todo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173", // dev frontend
+                        "https://todo-webservice.onrender.com" // prod if split
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
